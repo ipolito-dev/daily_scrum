ca0b3a32dbbe8234662e983a03f33552121c4b69
ipolito21 <ipolitodevelopes@gmail.com>
2022-02-22 18:21:50 -0300

    removed unnecessary codes

--- lib/data/datasources/auth_datasource/api_auth_datasource_impl.dart
+++ lib/data/datasources/auth_datasource/api_auth_datasource_impl.dart
@@ -1,27 +1,25 @@
 import 'package:daily_scrum/core/services/rest_client.dart';
 import 'package:daily_scrum/data/datasources/auth_datasource/i_auth_datasource.dart';
 import 'package:daily_scrum/data/models/logged_user_model.dart';
-import 'package:daily_scrum/domain/entities/logged_user_entity.dart';
+import 'package:daily_scrum/domain/errors/failure_errors.dart';
 import 'package:daily_scrum/domain/usecases/login_usecase.dart';
-import 'package:dio/dio.dart';
 
 class ApiAuthDatasourceImpl implements IAuthDatasource {
-  final Dio _dio = Dio(restClient.options);
+  final RestClient restClient;
+
+  ApiAuthDatasourceImpl(this.restClient);
+
   @override
-  Future<LoggedUserEntity> login(CredentialParams params) async {
-    // try {
-    final data = {
+  Future<LoggedUserModel> login(CredentialParams params) async {
+    final body = {
       'email': params.email,
       'password': params.password,
     };
-    var response = await _dio.post('/login', data: data);
-    print(response);
-    return LoggedUserModel.fromMap(response.data[0]);
-    // } on DioError catch (e) {
-    //   ClientStringCodeErrors treatedErrors =
-    //       ClientStringCodeErrors.automaticMessages(
-    //           messageToBeReplicated: "usuário não encontrado", dioError: e);
-    //   throw StatusException().build(e.response?.statusCode ?? 0, treatedErrors);
-    // }
+    var response = await restClient.post('/login', data: body);
+    if (response.statusCode == 200) {
+      return LoggedUserModel.fromMap(response.data[0]);
+    } else {
+      throw DatasourceError("");
+    }
   }
 }
