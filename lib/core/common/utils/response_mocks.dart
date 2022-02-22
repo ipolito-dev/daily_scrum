19563cfed0b134438c8862f63ef6a39e402e7679
ipolito21 <ipolitodevelopes@gmail.com>
2022-02-22 18:21:03 -0300

    add response mocks

---
+++ lib/core/common/utils/response_mocks.dart
@@ -0,0 +1,14 @@
+const apiLoginResult = """ 
+[
+ {
+  "name": "ipolito",
+  "email": "ipolito@gmail.com",
+  "id": "1"
+ },
+ {
+  "name": "ipolito",
+  "email": "ipolito@gmail.com",
+  "id": "2"
+ }
+]
+""";
