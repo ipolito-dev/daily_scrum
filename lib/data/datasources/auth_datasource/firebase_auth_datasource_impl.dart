e52ab68c2af86c59fc21272c2644fbd59a0dbe93
ipolito21 <ipolitodevelopes@gmail.com>
2022-02-22 18:26:43 -0300

    refactor in firebase auth datasource impl

--- lib/data/datasources/auth_datasource/firebase_auth_datasource_impl.dart
+++ lib/data/datasources/auth_datasource/firebase_auth_datasource_impl.dart
@@ -1,13 +1,14 @@
 
 import 'package:daily_scrum/data/datasources/auth_datasource/i_auth_datasource.dart';
+import 'package:daily_scrum/data/models/logged_user_model.dart';
 import 'package:daily_scrum/domain/usecases/login_usecase.dart';
-import 'package:daily_scrum/domain/entities/logged_user_entity.dart';
 
 class FirebaseAuthDatasourceImpl implements IAuthDatasource{
   @override
-  Future<LoggedUserEntity> login(CredentialParams params) {
+  Future<LoggedUserModel> login(CredentialParams params) {
     // TODO: implement login
     throw UnimplementedError();
   }
+
   
 }
