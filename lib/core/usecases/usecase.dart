0363727639c470f4ef7220d1308bd9bdb9f8d3a8
ipolito21 <ipolitodeveloper@gmail.com>
2022-03-16 04:04:27 -0300

    refactor in UseCaseFuture generic for requered params

--- lib/core/usecases/usecase.dart
+++ lib/core/usecases/usecase.dart
@@ -1,9 +1,9 @@
 import 'package:dartz/dartz.dart';
 
 abstract class UseCaseFuture<E extends Exception, T, P> {
-  Future<Either<E, T>> call({P? params});
+  Future<Either<E, T>> call({required P params});
 }
 
 abstract class UseCaseSync<E extends Exception, T, P> {
-  Either<E, T> call({P? params});
+  Either<E, T> call({required P params});
 }
